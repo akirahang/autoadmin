4b16879fe84604277b82bbdd81f3d5b3edf4c30e
austinhang <91672423+akirahang@users.noreply.github.com>
2024-11-19 16:44:51 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# 引用 functions.sh、docker_functions.sh 和 crontab.sh
-source ./env_setup/functions.sh
-source ./env_setup/docker_functions.sh
-source ./env_setup/crontab.sh
-source ./env_setup/vpn_functions.sh
-source ./env_setup/webdav_functions.sh
-
 # 主菜单函数
 show_main_menu() {
     while true; do
@@ -19,9 +10,10 @@ show_main_menu() {
         echo "3. Cron 任务管理"
         echo "4. WebDAV 挂载"
         echo "5. 科学上网管理"
-        echo "6. 退出"
+        echo "6. 内网穿透管理"            # 新增内网穿透管理选项
+        echo "7. 退出"
         echo "==============================="
-        read -p "请选择一个选项 (1-6): " choice
+        read -p "请选择一个选项 (1-7): " choice
 
         case $choice in
             1) show_docker_menu ;;          # Docker 管理菜单
@@ -29,11 +21,9 @@ show_main_menu() {
             3) cron_task_menu ;;            # Cron 任务管理
             4) mount_webdav_menu ;;         # WebDAV 挂载
             5) vpn_menu ;;                  # 科学上网管理
-            6) echo "退出脚本"; exit 0 ;;   # 退出脚本
+            6) port_forwarding_menu ;;        # 内网穿透管理菜单
+            7) echo "退出脚本"; exit 0 ;;   # 退出脚本
             *) echo "无效选项，请重试"; sleep 2 ;;
         esac
     done
 }
-
-# 启动主菜单
-show_main_menu
