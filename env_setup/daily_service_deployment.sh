3d6b925a831bb3d9a9346ee84145215035f6f5da
Akira Takahashi <91672423+LogicNekoChan@users.noreply.github.com>
2024-11-19 22:39:19 +0800

    Update daily_service_deployment.sh

--- env_setup/daily_service_deployment.sh
+++ env_setup/daily_service_deployment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# 获取本机公网IP
+get_public_ip() {
+    public_ip=$(curl -s ifconfig.me)
+    echo $public_ip
+}
+
 # 服务容器的配置
 services=(
     "AdGuardHome - 广告过滤器"
@@ -31,43 +37,53 @@ deploy_service() {
     case "$service_name" in
         "AdGuardHome - 广告过滤器")
             docker run -d --name adguardhome -p 53:53 -p 3000:3000 adguard/adguardhome
-            echo "AdGuardHome 服务已部署"
+            echo "AdGuardHome 服务已部署，访问地址：http://$(get_public_ip):3000"
             ;;
         "Alist - 文件管理工具")
             docker run -d --name alist -p 5244:5244 -p 6800:6800 xhofe/alist-aria2
-            echo "Alist 服务已部署"
+            echo "Alist 服务已部署，访问地址：http://$(get_public_ip):5244"
             ;;
         "Calibre Web - 电子书管理工具")
             docker run -d --name calibre-web -p 8083:8083 lscr.io/linuxserver/calibre-web
-            echo "Calibre Web 服务已部署"
+            echo "Calibre Web 服务已部署，访问地址：http://$(get_public_ip):8083"
             ;;
         "qBittorrent - 下载工具")
             docker run -d --name qbittorrent -p 8080:8080 -p 6881:6881 lscr.io/linuxserver/qbittorrent
-            echo "qBittorrent 服务已部署"
+            echo "qBittorrent 服务已部署，访问地址：http://$(get_public_ip):8080"
             ;;
         "Qinglong - 自动化脚本")
             docker run -d --name qinglong -p 5700:5700 whyour/qinglong
-            echo "Qinglong 服务已部署"
+            echo "Qinglong 服务已部署，访问地址：http://$(get_public_ip):5700"
             ;;
         "Vaultwarden - 密码管理")
-            docker run -d --name vaultwarden -p 86:80 vaultwarden/server:latest
-            echo "Vaultwarden 服务已部署"
+            # 提示用户输入数据库相关信息
+            read -p "请输入 Vaultwarden 数据库用户名: " db_user
+            read -sp "请输入 Vaultwarden 数据库密码: " db_password
+            echo ""
+            read -p "请输入数据库的主机 IP (默认为本机 IP): " db_host
+            db_host="${db_host:-$(get_public_ip)}"  # 默认使用本机 IP
+            
+            # 启动 Vaultwarden 服务
+            docker run -d --name vaultwarden -p 86:80 \
+                -e DATABASE_URL=postgresql://$db_user:$db_password@$db_host:5432/vaultwarden_db \
+                vaultwarden/server:latest
+            echo "Vaultwarden 服务已部署，访问地址：http://$(get_public_ip):86"
             ;;
         "Photoprism - 照片管理工具")
             docker run -d --name photoprism -p 2342:2342 photoprism/photoprism
-            echo "Photoprism 服务已部署"
+            echo "Photoprism 服务已部署，访问地址：http://$(get_public_ip):2342"
             ;;
         "Vocechat - 聊天工具")
             docker run -d --name vocechat -p 3019:3000 privoce/vocechat-server:latest
-            echo "Vocechat 服务已部署"
+            echo "Vocechat 服务已部署，访问地址：http://$(get_public_ip):3019"
             ;;
         "WordPress - 网站")
             docker run -d --name wordpress -p 8089:80 wordpress
-            echo "WordPress 服务已部署"
+            echo "WordPress 服务已部署，访问地址：http://$(get_public_ip):8089"
             ;;
         "Synctv - 文件同步")
             docker run -d --name synctv -p 8092:8080 synctvorg/synctv:latest
-            echo "Synctv 服务已部署"
+            echo "Synctv 服务已部署，访问地址：http://$(get_public_ip):8092"
             ;;
         *)
             echo "未知服务，请重新选择。"
@@ -106,4 +122,3 @@ daily_service_deployment_menu() {
 
 # 启动部署菜单
 daily_service_deployment_menu
-
