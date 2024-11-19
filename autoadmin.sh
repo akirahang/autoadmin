47cdbc5ede34f306d818ca44f1898f06979602fc
austinhang <91672423+akirahang@users.noreply.github.com>
2024-11-19 10:42:00 +0800

    Update autoadmin.sh

--- autoadmin.sh
+++ autoadmin.sh
@@ -4,7 +4,7 @@
 REPO_URL="https://github.com/akirahang/autoadmin.git"
 
 # 设置根目录下的目标文件夹名
-TARGET_DIR="/root/autoadmin"  # 更正为 /root/autoadmin
+TARGET_DIR="/root/autoadmin"
 
 # 切换到 root 用户的根目录
 cd /root || { echo "无法切换到根目录"; exit 1; }
@@ -19,6 +19,14 @@ fi
 # 克隆仓库
 echo "正在克隆仓库..."
 git clone "$REPO_URL" || { echo "克隆仓库失败"; exit 1; }
+
+# 给克隆的目录授予最高权限
+echo "正在授予 $TARGET_DIR 目录最高权限..."
+sudo chmod -R 777 "$TARGET_DIR" || { echo "无法授予权限"; exit 1; }
+
+# 确保所有者为 root
+echo "确保目录所有者为 root 用户..."
+sudo chown -R root:root "$TARGET_DIR" || { echo "无法更改所有者"; exit 1; }
 
 # 进入 autoadmin 文件夹
 cd "$TARGET_DIR" || { echo "进入 $TARGET_DIR 失败"; exit 1; }
